e283edf2d714f0d75eb57b60e6cc8e33b7630fae
cb201 <helloworld@csu.edu.cn>
2017-11-06 20:39:48 +0800

    add cross-validation function<2>

--- main/main.py
+++ main/main.py
@@ -38,49 +38,48 @@ with tf.Session() as sess:
         if i % display_num == 0:
             print("iter %d:" % (i + 1), loss_val)
             break
-                    print("iter %d:" % (i + 1), loss_val)
+            print("iter %d:" % (i + 1), loss_val)
 
-        mRNAseq = master.get_mRNA()
-        print('begin to training mRNA Autoencoder, sample number:', len(mRNAseq))
-        for j in range(mRNA_auto_epoch):
-            print('mRNA_auto_epoch:%d/%d' % (j + 1, mRNA_auto_epoch))
-            random.shuffle(mRNAseq)
-            for i, index in enumerate(range(0, len(mRNAseq), batch_num)):
-                x = mRNAseq[index:index + batch_num]
-                seq2 = master.transpose_seq(x)
-                loss_val, _ = sess.run([m_ae.loss, m_ae.train], {model.input_seq2: seq2})
-                if i % display_num == 0:
-                    print("iter %d:" % (i + 1), loss_val)
+    mRNAseq = master.get_mRNA()
+    print('begin to training mRNA Autoencoder, sample number:', len(mRNAseq))
+    for j in range(mRNA_auto_epoch):
+        print('mRNA_auto_epoch:%d/%d' % (j + 1, mRNA_auto_epoch))
+        random.shuffle(mRNAseq)
+        for i, index in enumerate(range(0, len(mRNAseq), batch_num)):
+            x = mRNAseq[index:index + batch_num]
+            seq2 = master.transpose_seq(x)
+            loss_val, _ = sess.run([m_ae.loss, m_ae.train], {model.input_seq2: seq2})
+            if i % display_num == 0:
+                print("iter %d:" % (i + 1), loss_val)
 
-        print('begin to training RNN-Model, training dataset:', len(master.train_data))
-        for j in range(Pred_rnn_epoch):
-            print('Pred_rnn_epoch:%d/%d' % (j + 1, Pred_rnn_epoch))
-            random.shuffle(master.train_data)
-            for i, index in enumerate(range(0, len(master.train_data), batch_num)):
-                x = master.train_data[index:index + batch_num]
-                seq1, seq2, ys = master.transpose(x)
-                accuracy, loss_val, _ = sess.run(
-                    [model.accuracy, model.mean_loss, model.optimizer],
-                    {model.input_seq1: seq1, model.input_seq2: seq2,
-                     model.labels: ys})
-                if i % display_num == 0:
-                    print("iter %d:" % (i + 1), loss_val, "accuracy %.3f:" % accuracy)
+    print('begin to training RNN-Model, training dataset:', len(master.train_data))
+    for j in range(Pred_rnn_epoch):
+        print('Pred_rnn_epoch:%d/%d' % (j + 1, Pred_rnn_epoch))
+        random.shuffle(master.train_data)
+        for i, index in enumerate(range(0, len(master.train_data), batch_num)):
+            x = master.train_data[index:index + batch_num]
+            seq1, seq2, ys = master.transpose(x)
+            accuracy, loss_val, _ = sess.run(
+                [model.accuracy, model.mean_loss, model.optimizer],
+                {model.input_seq1: seq1, model.input_seq2: seq2,
+                 model.labels: ys})
+            if i % display_num == 0:
+                print("iter %d:" % (i + 1), loss_val, "accuracy %.3f:" % accuracy)
 
-        print('begin to testing ALL-Model', len(master.test_data))
-        result_score = []
-        result_pred = []
-        for i, index in enumerate(range(0, len(master.test_data), batch_num)):
-            x = master.test_data[index:index + batch_num]
-            seq1, seq2 = master.transpose_nolabel(x)
-            scores, predictions = sess.run([model.scores, model.predictions],
-                                           {model.input_seq1: seq1, model.input_seq2: seq2})
-            result_pred += list(predictions)
-            result_score += list(scores)
-        print("result_score=", len(result_score))
-        with open(datapath + '/result_n%d.txt' % (datalabel + 1), 'w') as result, open(testpath, 'r') as file:
-            for row, line in enumerate(file.readlines()):
-                result.write(line.strip() + " " + "%.8f" % result_score[row][1] + "\n")
-
+    print('begin to testing ALL-Model', len(master.test_data))
+    result_score = []
+    result_pred = []
+    for i, index in enumerate(range(0, len(master.test_data), batch_num)):
+        x = master.test_data[index:index + batch_num]
+        seq1, seq2 = master.transpose_nolabel(x)
+        scores, predictions = sess.run([model.scores, model.predictions],
+                                       {model.input_seq1: seq1, model.input_seq2: seq2})
+        result_pred += list(predictions)
+        result_score += list(scores)
+    print("result_score=", len(result_score))
+    with open(datapath + '/result_n%d.txt' % (datalabel + 1), 'w') as result, open(testpath, 'r') as file:
+        for row, line in enumerate(file.readlines()):
+            result.write(line.strip() + " " + "%.8f" % result_score[row][1] + "\n")
 
 for i in range(10):
     datapath = "datasets/deeptarget%d" % (i + 1)
